d0e5b49b47f776b454278fcb299b410112de6873
Esben Tind <esbentind@gmail.com>
2024-09-16 16:31:42 +0000

    Removed auto generated dummy code

--- src/Repository/ProductRatingRepository.php
+++ src/Repository/ProductRatingRepository.php
@@ -16,28 +16,4 @@ class ProductRatingRepository extends ServiceEntityRepository
         parent::__construct($registry, ProductRating::class);
     }
 
-    //    /**
-    //     * @return ProductRating[] Returns an array of ProductRating objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('p.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?ProductRating
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
