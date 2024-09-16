5539cfce8848a8569652f0530a5250f17084874a
Esben Tind <esbentind@gmail.com>
2024-09-16 12:28:45 +0000

    Refactored filter model to be just a constructor with public properties

--- src/Model/Filter.php
+++ src/Model/Filter.php
@@ -3,29 +3,9 @@ namespace App\Model;
 
 class Filter
 {
-    private string $key;
-    private string $operator;
-    private string $value;
-
-    public function __construct(string $key, string $operator, string $value)
-    {
-        $this->key = $key;
-        $this->operator = $operator;
-        $this->value = $value;
-    }
-
-    public function getKey(): string
-    {
-        return $this->key;
-    }
-
-    public function getOperator(): string
-    {
-        return $this->operator;
-    }
-
-    public function getValue(): string
-    {
-        return $this->value;
-    }
+    public function __construct(
+        public string $key, 
+        public string $operator, 
+        public string $value)
+    {}
 }
